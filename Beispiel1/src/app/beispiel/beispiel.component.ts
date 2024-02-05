afe15ec780623d27a02220e2f12317d47cd1f087
drjofu <drjofu@web.de>
2024-02-05 12:18:50 +0100

    Tag 1

---
+++ Beispiel1/src/app/beispiel/beispiel.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beispiel',
+  templateUrl: './beispiel.component.html',
+  styleUrl: './beispiel.component.css',
+})
+export class BeispielComponent {
+  info: string = 'nur zur Info';
+  zahl = 1234;
+  fs = '40px';
+
+  text?: string;
+
+  buttonClick() {
+    // let t = 'irgendwas';
+    // this.info = t + ' anderes';
+
+    this.info = `die Zahl ist ${this.zahl}, text: ${this.info}`;
+    this.text = 'neuer Text';
+  }
+}
